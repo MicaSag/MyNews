1583b509b6b1bb4c82220feef504d2afca0d320a
Michael <sagotmickael@neuf.fr>
2018-04-24 22:09:25 +0200

    Implementation of the tree MVC + Utils case

--- app/src/main/java/com/android/sagot/mynews/Controllers/Activities/MainActivity.java
+++ app/src/main/java/com/android/sagot/mynews/Controllers/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.android.sagot.mynews;
+package com.android.sagot.mynews.Controllers.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.android.sagot.mynews.R;
 
 public class MainActivity extends AppCompatActivity {
 
