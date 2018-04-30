972d7c5a5ac15f96c3829ff9a4ae4a430e81aadf
Michael <sagotmickael@neuf.fr>
2018-04-30 17:30:03 +0200

    Implementation a ViewPager in the FrameLayout of the Navigation Drawer Create Adapter and Fragment

--- app/src/main/java/com/android/sagot/mynews/Controllers/Activities/MainActivity.java
+++ app/src/main/java/com/android/sagot/mynews/Controllers/Activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.android.sagot.mynews.Controllers.Activities;
 
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,11 +10,12 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.android.sagot.mynews.Adapters.PageAdapter;
 import com.android.sagot.mynews.R;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    //FOR DESIGN
+    //NAVIGATION DRAWER Design
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
@@ -23,30 +25,53 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 6 - Configure all views
-
+        // NAVIGATION DRAWER
+        // Configure all views of the Navigation Drawer
         this.configureToolBar();
-
         this.configureDrawerLayout();
-
         this.configureNavigationView();
+
+        // VIEW PAGER
+        //Configure ViewPager
+        this.configureViewPager();
     }
 
-    @Override
-    public void onBackPressed() {
-        // 5 - Handle back click to close menu
-        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            this.drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+    // ---------------------------------------------------------------------------------------------
+    //                                     NAVIGATION DRAWER
+    // ---------------------------------------------------------------------------------------------
+
+    // ---------------------
+    // CONFIGURATION
+    // ---------------------
+
+    // Configure Toolbar
+    private void configureToolBar(){
+        this.toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
     }
 
+    // Configure Drawer Layout
+    private void configureDrawerLayout(){
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
 
+    // Configure NavigationView
+    private void configureNavigationView() {
+        this.navigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    // ---------------------
+    // ACTIONS
+    // ---------------------
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
 
-        // 4 - Handle Navigation Item Click
+        // Handle Navigation Item Click
         int id = item.getItemId();
 
         /*switch (id){
@@ -65,27 +90,30 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
+    @Override
+    public void onBackPressed() {
+        // Close the menu so open and if the touch return is pushed
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    //                                     VIEW PAGER
+    // ---------------------------------------------------------------------------------------------
+
     // ---------------------
     // CONFIGURATION
     // ---------------------
 
-    // 1 - Configure Toolbar
-    private void configureToolBar(){
-        this.toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
-        setSupportActionBar(toolbar);
-    }
-
-    // 2 - Configure Drawer Layout
-    private void configureDrawerLayout(){
-        this.drawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-    }
-
-    // 3 - Configure NavigationView
-    private void configureNavigationView() {
-        this.navigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+    private void configureViewPager(){
+        // Get ViewPager from layout
+        ViewPager pager = (ViewPager)findViewById(R.id.activity_main_viewpager);
+        // Set Adapter PageAdapter and glue it together
+        pager.setAdapter(new PageAdapter(getSupportFragmentManager(),
+                getResources().getIntArray(R.array.colorPagesViewPager)) {
+        });
     }
 }
