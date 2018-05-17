2d0dc3e60e1141110c007bc5478990393a77f1c4
Michael <sagotmickael@neuf.fr>
2018-05-17 18:13:38 +0200

    Additions of icons for items menu drawer ans implements its.

--- app/src/main/java/com/android/sagot/mynews/Controllers/Activities/SearchActivity.java
+++ app/src/main/java/com/android/sagot/mynews/Controllers/Activities/SearchActivity.java
@@ -4,15 +4,29 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.android.sagot.mynews.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SearchActivity extends AppCompatActivity {
+
+    // Adding @BindView in order to indicate to ButterKnife to get & serialise it
+    @BindView(R.id.activity_search_text) TextView mTextview;
+    @BindView(R.id.activity_search_spinner_date_begin) Spinner mSpinnerDateBegin;
+    @BindView(R.id.activity_search_spinner_date_end) Spinner mSpinnerDateEnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        // Get & serialise all views
+        ButterKnife.bind(this);
+
         // Configuring Toolbar
         this.configureToolbar();
     }
