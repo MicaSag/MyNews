28cc3da77bf2f4c12eb78b0f63589340d2d1641d
Michaël SAGOT <sagotmickael@neuf.fr>
2018-05-28 14:39:32 +0200

    Update DataModel.java
    _ Add comments

--- app/src/main/java/com/android/sagot/mynews/Models/DataModel.java
+++ app/src/main/java/com/android/sagot/mynews/Models/DataModel.java
@@ -2,12 +2,14 @@ package com.android.sagot.mynews.Models;
 
 import java.util.Date;
 
-public class DataModel {
-    private SearchCriteria mSearchCriteria;  // Save the search Criteria
-    private NotificationsCriteria mNotificationsCriteria; // Save the notifications criteria
-    // Save the publication date of the most recent news found matching the notification criteria
-    private Date mLastNewsDate;
+/**
+ *  Data group used in the application and saved in the SharedPreferences
+ */
 
+public class DataModel {
+    private SearchCriteria mSearchCriteria;               // Data group used in the layout "activity_search.xml/category.xml"
+    private NotificationsCriteria mNotificationsCriteria; // Data group used in the layout "activity_notifications.xml/category.xml"
+    private Date mLastNewsDate; // Save the publication date of the most recent news found matching the notification criteria
 
     public SearchCriteria getSearchCriteria() {
         return mSearchCriteria;
