20c006cd1851d6ecff5256d2bd337fbf49893e75
Michaël SAGOT <sagotmickael@neuf.fr>
2018-06-04 12:34:15 +0200

    Update TopStoriesFragment.java
    Document the code a little better

--- app/src/main/java/com/android/sagot/mynews/Controllers/Fragments/TopStoriesFragment.java
+++ app/src/main/java/com/android/sagot/mynews/Controllers/Fragments/TopStoriesFragment.java
@@ -1,6 +1,5 @@
 package com.android.sagot.mynews.Controllers.Fragments;
 
-
 import android.os.Bundle;
 import android.util.Log;
 
@@ -16,7 +15,6 @@ import java.util.Collections;
 import java.util.List;
 
 import io.reactivex.observers.DisposableObserver;
-
 /**
  *  TopStories FRAGMENT
  */
@@ -31,22 +29,21 @@ public class TopStoriesFragment extends BaseNewsFragment {
 
     // Method that will create a new instance of TopStoriesFragment, and add data to its bundle.
     public static TopStoriesFragment newInstance(int tabLayoutPosition) {
-
         // Create new fragment
         TopStoriesFragment fragment = new TopStoriesFragment();
-
         // Create bundle and add it some data
         Bundle args = new Bundle();
         args.putInt(BUNDLE_TAB_LAYOUT_POSITION, tabLayoutPosition);
         fragment.setArguments(args);
-
         return fragment;
     }
     
     // --------------
     //    ( IN )
     // --------------
-    // Get the list of TopStories news saved in the Model
+    // BASE METHOD Implementation
+    // Get the list of Top Stories news saved in the Model
+    // CALLED BY BASE METHOD 'onCreateView(...)'
     @Override
     protected List<NYTimesNews> getListNYTimesNewsOfTheModel() {
         return Model.getInstance().getListTopStoriesNews();
@@ -55,39 +52,39 @@ public class TopStoriesFragment extends BaseNewsFragment {
     // -------------------
     // HTTP (RxJAVA)
     // -------------------
-    /**
-     *  Execute Stream " NYTimesStreams.streamFetchTopStories "
-     */
+    //  BASE METHOD Implementation
+    //  Execute Stream " NYTimesStreams.streamFetchTopStories "
+    // CALLED BY BASE METHOD 'updateUIWithListOfNews((...)'
     @Override
     protected void executeHttpRequestWithRetrofit(){
 
         // Execute the stream subscribing to Observable defined inside NYTimesStreams
         String section = "home";                      // Section of the new to return
-        this.mDisposable = NYTimesStreams.streamFetchTopStories(section, api_key).subscribeWith(new DisposableObserver<NYTimesTopStories>() {
+        this.mDisposable = NYTimesStreams.streamFetchTopStories(section, api_key)
+                .subscribeWith(new DisposableObserver<NYTimesTopStories>() {
             @Override
             public void onNext(NYTimesTopStories topStories) {
                 Log.e("TAG","On Next");
-                // Update UI with list of TopStories news
+                // CALL BASE METHOD : Update UI with list of news
                 updateUIWithListOfNews(topStories);
             }
-
             @Override
             public void onError(Throwable e) {
                 Log.e("TAG","On Error"+Log.getStackTraceString(e));
+                // CALL BASE METHOD : Generate a toast Message if error during Downloading
                 updateUIWhenErrorHTTPRequest();
             }
-
             @Override
-            public void onComplete() {
-                Log.e("TAG","On Complete !!");
-            }
+            public void onComplete() { Log.e("TAG","On Complete !!"); }
         });
     }
     
-    // -------------------
-    //     UPDATE UI
-    // -------------------
+    // --------------
+    //   UPDATE UI
+    // --------------
+    // BASE METHOD Implementation
     // Create list of news to display
+    // CALLED BY BASE METHOD 'updateUIWithListOfNews(...)
     @Override
     protected void createListNYTimesNews(Object news) {
         NYTimesNewsList.createListArticleTopStories(mListNYTimesNews,(NYTimesTopStories)news);  
@@ -96,7 +93,9 @@ public class TopStoriesFragment extends BaseNewsFragment {
     // --------------
     //    ( OUT )
     // --------------
-    // Save the list of TopStories in the Model
+    // BASE METHOD Implementation
+    // Save the list of Business in the Model
+    // CALLED BY BASE METHOD 'updateUIWithListOfNews(...)
     @Override
     protected void setListNYTimesNewsInTheModel(List<NYTimesNews> newsList) {
         Model.getInstance().setListTopStoriesNews(newsList);
