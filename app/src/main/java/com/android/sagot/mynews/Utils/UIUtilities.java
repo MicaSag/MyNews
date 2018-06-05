e5cda0e038229c697ffea1ca6194302c9f5c7d3f
Michaël SAGOT <sagotmickael@neuf.fr>
2018-06-05 12:31:53 +0200

    Update UIUtilities.java
    Add class DividerItemDecoration

--- app/src/main/java/com/android/sagot/mynews/Utils/UIUtilities.java
+++ app/src/main/java/com/android/sagot/mynews/Utils/UIUtilities.java
@@ -7,10 +7,10 @@ import android.view.WindowManager;
 
 public class UIUtilities {
 
-    // ---------------------------------------------------------------------------------------------
-    //                           STATUS BAR Only SDK_VERSION > 21
-    // ---------------------------------------------------------------------------------------------
-
+    /**                      
+     * Method allowing to change the color of bar of status
+     * Only SDK_VERSION > 21
+     */
     // Change the color of the status bar
     public static void changeStatusBarColor(Activity content, int color) {
         if (Build.VERSION.SDK_INT >= 21) {
@@ -18,6 +18,50 @@ public class UIUtilities {
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(color);
+        }
+    }
+    
+    /* ----------------------------------------------------------------------- 
+     *  Class allowing to add a separator in various item from a recyclerview
+     */ 
+    public class DividerItemDecoration extends RecyclerView.ItemDecoration {
+
+    private static final int[] ATTRS = new int[]{android.R.attr.listDivider};
+
+    private Drawable divider;
+
+    /**
+     * Default divider will be used
+     */
+    public DividerItemDecoration(Context context) {
+        final TypedArray styledAttributes = context.obtainStyledAttributes(ATTRS);
+        divider = styledAttributes.getDrawable(0);
+        styledAttributes.recycle();
+    }
+
+    /**
+     * Custom divider will be used
+     */
+    public DividerItemDecoration(Context context, int resId) {
+        divider = ContextCompat.getDrawable(context, resId);
+    }
+
+    @Override
+    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = parent.getChildAt(i);
+
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+            int top = child.getBottom() + params.bottomMargin;
+            int bottom = top + divider.getIntrinsicHeight();
+
+            divider.setBounds(left, top, right, bottom);
+            divider.draw(c);
         }
     }
 }
