0527e26716599a6baa127b08e2f37f9691e962ed
Michael <sagotmickael@neuf.fr>
2018-06-07 23:43:47 +0200

    Delete mLastNewDate int the SavedModel

--- app/src/main/java/com/android/sagot/mynews/Models/SavedModel.java
+++ app/src/main/java/com/android/sagot/mynews/Models/SavedModel.java
@@ -11,7 +11,6 @@ public class SavedModel {
     private SearchCriteria mSearchCriteria;               // Data group used in the layout "activity_search.xml/category.xml"
     private NotificationsCriteria mNotificationsCriteria; // Data group used in the layout "activity_notifications.xml/category.xml"
     private List<String> mListUrlArticleRead;             // List of urls of articles already read
-    private Date mLastNewsDate; // Save the publication date of the most recent news found matching the notification criteria
 
     public SearchCriteria getSearchCriteria() {
         return mSearchCriteria;
@@ -35,13 +34,5 @@ public class SavedModel {
 
     public void setListUrlArticleRead(List<String> listUrlArticleRead) {
         mListUrlArticleRead = listUrlArticleRead;
-    }
-
-    public Date getLastNewsDate() {
-        return mLastNewsDate;
-    }
-
-    public void setLastNewsDate(Date lastNewsDate) {
-        mLastNewsDate = lastNewsDate;
     }
 }
