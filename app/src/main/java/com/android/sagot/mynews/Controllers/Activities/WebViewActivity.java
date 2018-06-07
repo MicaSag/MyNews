72264d2924df53ad8717dd35784b8d4f3e6b7512
Michaël SAGOT <sagotmickael@neuf.fr>
2018-06-07 10:32:57 +0200

    Update WebViewActivity.java

--- app/src/main/java/com/android/sagot/mynews/Controllers/Activities/WebViewActivity.java
+++ app/src/main/java/com/android/sagot/mynews/Controllers/Activities/WebViewActivity.java
@@ -69,6 +69,17 @@ public class WebViewActivity extends AppCompatActivity {
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
     }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        // Respond to the action bar's Up/Home button
+        case android.R.id.home:
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     private void applyColorTheme() {
         // Change Color of the Toolbar
