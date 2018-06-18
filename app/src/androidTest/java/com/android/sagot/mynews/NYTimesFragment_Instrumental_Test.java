4075ab96a794b62626324660c3450de024cadbfd
Michael <sagotmickael@neuf.fr>
2018-06-18 15:15:49 +0200

    Add instrumentation test : fetchNewsArticleSearch_I_Test()

--- app/src/androidTest/java/com/android/sagot/mynews/NYTimesFragment_Instrumental_Test.java
+++ app/src/androidTest/java/com/android/sagot/mynews/NYTimesFragment_Instrumental_Test.java
@@ -3,12 +3,16 @@ package com.android.sagot.mynews;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
+import com.android.sagot.mynews.Models.NYTimesStreams.ArticleSearch.NYTimesArticleSearch;
 import com.android.sagot.mynews.Models.NYTimesStreams.MostPopular.NYTimesMostPopular;
 import com.android.sagot.mynews.Models.NYTimesStreams.TopStories.NYTimesTopStories;
 import com.android.sagot.mynews.Utils.NYTimesStreams;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import io.reactivex.Observable;
 import io.reactivex.observers.TestObserver;
@@ -60,5 +64,31 @@ public class NYTimesFragment_Instrumental_Test {
         assertThat("The status of the Stream was read correctly", newsFetched.getStatus().equals("OK"));
         // Verify if Results Exist
         assertThat("Results exist in the Stream request", !newsFetched.getResults().equals(null));
+    }
+
+    @Test
+    public void fetchNewsArticleSearch_I_Test() throws Exception {
+        // Criteria of the query
+        Map<String, String> query =  new HashMap<>();
+        query.put("q", "Obama");
+        query.put("fq", "news_desk:( Sports , Business , Politics");
+        // Get the stream
+        Observable<NYTimesArticleSearch> observableNews =
+                NYTimesStreams.streamFetchArticleSearch("de9402ab67114b3c8f08f3d58562b310", query);
+        // Create a new TestObserver
+        TestObserver<NYTimesArticleSearch> testObserver = new TestObserver<>();
+        // Launch observable
+        observableNews.subscribeWith(testObserver)
+                .assertNoErrors()       // Check if no errors
+                .assertNoTimeout()      // Check if no Timeout
+                .awaitTerminalEvent();  // Await the stream terminated before continue
+
+        // Get list of news fetched
+        NYTimesArticleSearch newsFetched = testObserver.values().get(0);
+        Log.d("JUNIT_TEST", "fetchNewsTest:newsFetched.getNumResults() = "+newsFetched.getResponse());
+        // Verify if status: "OK"
+        assertThat("The status of the Stream was read correctly", newsFetched.getStatus().equals("OK"));
+        // Verify if Results Exist
+        assertThat("Results exist in the Stream request", !newsFetched.getResponse().equals(null));
     }
 }
