01f0c1a61cb69451cae1f09019655c00fde6709f
Michael <sagotmickael@neuf.fr>
2018-06-25 00:28:31 +0200

    Update AlarmManager

--- app/src/main/java/com/android/sagot/mynews/Controllers/Activities/NotificationsActivity.java
+++ app/src/main/java/com/android/sagot/mynews/Controllers/Activities/NotificationsActivity.java
@@ -184,10 +184,10 @@ public class NotificationsActivity extends BaseCriteriaActivity {
 
         Calendar cal = Calendar.getInstance();
         // If it is after noon then we add one day to the meter of release of the alarm
-        //if (cal.get(Calendar.HOUR_OF_DAY) > 12 ) cal.add(Calendar.DATE, 1);
+        if (cal.get(Calendar.HOUR_OF_DAY) > 12 ) cal.add(Calendar.DATE, 1);
         // The alarm next one will thus be at 12:00 am tomorrow
-        cal.set(Calendar.HOUR_OF_DAY, 20);
-        cal.set(Calendar.MINUTE, 54);
+        cal.set(Calendar.HOUR_OF_DAY, 12);
+        cal.set(Calendar.MINUTE, 00);
         cal.set(Calendar.SECOND, 00);
         cal.set(Calendar.MILLISECOND, 0);
         //The hour it milliseconds of release of the first alarm
